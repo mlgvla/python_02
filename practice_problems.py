dcc4bc8a05ae56ee45cc399891d6f52620320a01
Monica Gerard <mlgvla@gmail.com>
2023-07-05 11:29:48 -0400

    adds problem solutions

--- practice_problems.py
+++ practice_problems.py
@@ -6,7 +6,7 @@ and returns a new list containing the squares of those numbers.
 numbers = [1, 2, 3, 4, 5]
 
 def square_numbers(numbers):
-    pass
+    return [int**2 for int in numbers]
 
 result = square_numbers(numbers)
 print(result)  # Output: [1, 4, 9, 16, 25]
@@ -14,13 +14,14 @@ print(result)  # Output: [1, 4, 9, 16, 25]
 
 """
 Problem 2: Remove Vowels
-Write a program that takes a string as input and r
-emoves all the vowels from it, returning the modified string.
+Write a program that takes a string as input and 
+removes all the vowels from it, returning the modified string.
 """
 string = "Hello, World!"
 
 def remove_vowels(string):
-    pass
+    vowels = "aeiou"
+    return "".join([char for char in string if char.lower() not in vowels])
 
 result = remove_vowels(string)
 print(result)  # Output: "Hll, Wrld!"
@@ -35,7 +36,8 @@ list1 = [1, 2, 3, 4, 5]
 list2 = [4, 5, 6, 7, 8]
 
 def find_common_elements(list1, list2):
-    pass
+    return [element for element in list1 if element in list2]
+
 
 result = find_common_elements(list1, list2)
 print(result)  # Output: [4, 5]
@@ -46,11 +48,11 @@ Problem 4: Create a Dictionary from Two Lists
 Write a program that takes two lists, one with keys and the other with values, 
 and creates a dictionary using those lists.
 """
-keys = ['a', 'b', 'c']
+keys = ["a", "b", "c"]
 values = [1, 2, 3]
 
 def create_dictionary(keys, values):
-    pass
+    return {key: value for key, value in zip(keys, values)}
 
 result = create_dictionary(keys, values)
 print(result)  # Output: {'a': 1, 'b': 2, 'c': 3}
@@ -59,12 +61,12 @@ print(result)  # Output: {'a': 1, 'b': 2, 'c': 3}
 """
 Problem 5: Filter Even Numbers from a List
 Write a program that takes a list of numbers as input and 
-eturns a new list containing only the even numbers from the original list.
+returns a new list containing only the even numbers from the original list.
 """
 numbers = [1, 2, 3, 4, 5, 6]
 
 def filter_even_numbers(numbers):
-    pass
+    return [num for num in numbers if num % 2 == 0]
 
 result = filter_even_numbers(numbers)
 print(result)  # Output: [2, 4, 6]
@@ -78,7 +80,8 @@ containing the frequency of each word in the sentence.
 sentence = "The cat chased the mouse and the mouse chased its tail."
 
 def count_word_frequency(sentence):
-   pass
+    words = sentence.split()
+    return {word: words.count(word) for word in set(words)}
 
 result = count_word_frequency(sentence)
 print(result)
@@ -90,11 +93,10 @@ Problem 7: Get the Maximum Value in a Dictionary
 Write a program that takes a dictionary as input and 
 returns the maximum value in the dictionary.
 """
-dictionary = {'a': 10, 'b': 5, 'c': 15}
+dictionary = {"a": 10, "b": 5, "c": 15}
 
 def get_maximum_value(dictionary):
-    pass
+    return max(dictionary.values())
 
 result = get_maximum_value(dictionary)
 print(result)
-
