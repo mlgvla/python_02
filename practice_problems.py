93855e17722c49bb23664a3c1123ca5b2881435a
Monica Gerard <mlgvla@gmail.com>
2023-07-05 00:41:41 -0400

    adds practice_problems.py

---
+++ practice_problems.py
@@ -0,0 +1,106 @@
+"""
+Problem 1: Square Numbers
+Write a program that takes a list of numbers as input 
+and returns a new list containing the squares of those numbers.
+"""
+numbers = [1, 2, 3, 4, 5]
+
+def square_numbers(numbers):
+    pass
+
+result = square_numbers(numbers)
+print(result)  # Output: [1, 4, 9, 16, 25]
+
+
+
+"""
+Problem 2: Remove Vowels
+Write a program that takes a string as input and r
+emoves all the vowels from it, returning the modified string.
+"""
+string = "Hello, World!"
+
+def remove_vowels(string):
+    pass
+
+result = remove_vowels(string)
+print(result)  # Output: "Hll, Wrld!"
+
+
+
+"""
+Problem 3: Find Common Elements in Lists
+Write a program that takes two lists as input and returns a new list 
+containing only the common elements between the two lists.
+"""
+list1 = [1, 2, 3, 4, 5]
+list2 = [4, 5, 6, 7, 8]
+
+def find_common_elements(list1, list2):
+    pass
+
+result = find_common_elements(list1, list2)
+print(result)  # Output: [4, 5]
+
+
+
+"""
+Problem 4: Create a Dictionary from Two Lists
+Write a program that takes two lists, one with keys and the other with values, 
+and creates a dictionary using those lists.
+"""
+keys = ['a', 'b', 'c']
+values = [1, 2, 3]
+
+def create_dictionary(keys, values):
+    pass
+
+result = create_dictionary(keys, values)
+print(result)  # Output: {'a': 1, 'b': 2, 'c': 3}
+
+
+
+"""
+Problem 5: Filter Even Numbers from a List
+Write a program that takes a list of numbers as input and 
+eturns a new list containing only the even numbers from the original list.
+"""
+numbers = [1, 2, 3, 4, 5, 6]
+
+def filter_even_numbers(numbers):
+    pass
+
+result = filter_even_numbers(numbers)
+print(result)  # Output: [2, 4, 6]
+
+
+
+"""
+Problem 6: Count Word Frequency in a Sentence
+Write a program that takes a sentence as input and returns a dictionary
+containing the frequency of each word in the sentence.
+"""
+sentence = "The cat chased the mouse and the mouse chased its tail."
+
+def count_word_frequency(sentence):
+   pass
+
+result = count_word_frequency(sentence)
+print(result)
+# Output: {'The': 1, 'mouse': 2, 'and': 1, 'cat': 1, 'its': 1, 'tail.': 1, 'chased': 2, 'the': 2}
+
+
+
+"""
+Problem 7: Get the Maximum Value in a Dictionary
+Write a program that takes a dictionary as input and 
+returns the maximum value in the dictionary.
+"""
+dictionary = {'a': 10, 'b': 5, 'c': 15}
+
+def get_maximum_value(dictionary):
+    pass
+
+result = get_maximum_value(dictionary)
+print(result)
+
